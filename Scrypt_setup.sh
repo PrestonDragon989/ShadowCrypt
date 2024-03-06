dff137829d6a7e639dde801702533d8d20711629
PrestonDragon <122578013+PrestonDragon989@users.noreply.github.com>
2024-03-06 06:31:49 +0000

    Worked on subparsers

--- Scrypt_setup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Set executable permissions for encryption/decrypter scripts
-chmod +x Scrypt.encrypter
-chmod +x Scrypt.decrypter
-
-# Set executable permissions for deletion/purge script
-chmod +x Scrypt.purge
